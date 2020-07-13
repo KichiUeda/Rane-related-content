eb3475a51ef632609142f0ccf2e63ffad0eabea3
Rane Wallin <github@blasph.me>
2020-07-13 16:31:25 -0700

    Stub basic node server and static html page

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(helmet.noSniff());
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.status(200).send("index");
+});
+
+module.exports = app;
