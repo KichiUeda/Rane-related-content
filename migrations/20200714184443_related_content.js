4be1937560a56c4708d115209d64d76035de6571
Rane Wallin <github@blasph.me>
2020-07-14 20:50:07 -0700

    Setting up migrations

--- migrations/20200714184443_related_content.js
+++ migrations/20200714184443_related_content.js
@@ -1,3 +1,11 @@
-exports.up = function (knex) {};
+exports.up = async (knex) => {
+  await knex.schema.createTable("related_content", (tbl) => {
+    tbl.increments();
+    tbl.integer("product_id");
+    tbl.integer("related_id");
+  });
+};
 
-exports.down = function (knex) {};
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("related_content");
+};
