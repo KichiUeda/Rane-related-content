da882d137f9699c5f1a0fc89d6ae890cbc0e1666
Rane Wallin <github@blasph.me>
2020-07-18 08:26:48 -0700

    First test for fe route

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(helmet.noSniff());
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(200).send("index");
--- tests/server.test.js
+++ tests/server.test.js
@@ -5,22 +5,30 @@ const { createInstance } = require("axios-test-instance");
 const server = require("../server/server.js");
 
 let instance;
-
-beforeAll(async () => {
-  instance = await createInstance(server);
-});
-
-afterAll(async () => {
-  await instance.close();
-});
-
-describe("GET /related/:product_id", () => {
-  xtest("returns status 404 when passed an invalid id", async () => {
-    let { status } = await instance.get("/related/122");
-    expect(status).toBe(404);
+describe("backend tests", () => {
+  beforeAll(async () => {
+    instance = await createInstance(server);
   });
-  test("returns status 400 when not passed an id", async () => {
-    let { status } = await instance.get("/related");
-    expect(status).toBe(400);
+
+  afterAll(async () => {
+    await instance.close();
+  });
+
+  describe("GET /related/:product_id", () => {
+    xtest("returns status 404 when passed an invalid id", async () => {
+      let { status } = await instance.get("/related/122");
+      expect(status).toBe(404);
+    });
+    test("returns status 400 when not passed an id", async () => {
+      let { status } = await instance.get("/related");
+      expect(status).toBe(400);
+    });
+  });
+
+  describe("GET /related/febits", () => {
+    test("returns 400 when not passed an id", async () => {
+      let { status } = await instance.get("/related/febits");
+      expect(status).toBe(400);
+    });
   });
 });
