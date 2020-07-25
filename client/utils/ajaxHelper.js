5f32d3516ec6bcdcefa73db4887470aeb36362e7
Rane Wallin <github@blasph.me>
2020-07-25 10:40:13 -0700

    Domain switching

--- client/utils/ajaxHelper.js
+++ client/utils/ajaxHelper.js
@@ -4,13 +4,18 @@ const RELATED_EP = "related/";
 const RELATED_FE_EP = RELATED_EP + "febits/";
 
 export const getCardData = async () => {
+  const API =
+    window.location.origin ===
+    "http://ec2-54-224-38-115.compute-1.amazonaws.com:3007"
+      ? "/"
+      : "http://ec2-54-224-38-115.compute-1.amazonaws.com:3007/";
   const product_id = window.location.pathname.replace("/", "");
-  let relatedProducts = await axios.get(RELATED_EP + product_id);
+  let relatedProducts = await axios.get(API + RELATED_EP + product_id);
   let cardData = [];
   relatedProducts = relatedProducts.data;
 
   for (let i = 0; i < relatedProducts.length; i++) {
-    let card = await axios.get(RELATED_FE_EP + relatedProducts[i]);
+    let card = await axios.get(API + RELATED_FE_EP + relatedProducts[i]);
 
     cardData.push(card.data);
   }
