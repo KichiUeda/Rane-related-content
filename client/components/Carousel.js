b02f9f81d6a4b47ea5b9d143d94f9f637443f59b
Rane Wallin <github@blasph.me>
2020-07-27 17:24:18 -0700

    Fixing some styles

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -2,26 +2,41 @@ import React from "react";
 import ReactLoading from "react-loading";
 import styled from "styled-components";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-
+import "../reset.css";
 import { RowWrapper, ColWrapper } from "../GlobalStyles";
 import Card from "./Card";
 
 const Carousel = ({ cardData }) => {
   return (
     <CarouselWrapper>
-      <SectionHead>Related content</SectionHead>
-      <RowWrapper margin="0 70px" data-testid="carousel-component">
-        {/* <LeftArrow aria-label="Scroll left">
+      <ColWrapper margin="15px 0 0 0">
+        <ColWrapper
+          justify="flex-start"
+          alignItems="flex-start"
+          alignContent="flex-start"
+          margin="0 auto"
+        >
+          <SectionHead>Related content</SectionHead>
+          <RowWrapper
+            justify-content="flex-start"
+            alignContent="flex-start"
+            alignItems="flex-start"
+            data-testid="carousel-component"
+          >
+            {/* <LeftArrow aria-label="Scroll left">
           <FiChevronLeft />
         </LeftArrow> */}
-        {(cardData.length &&
-          cardData.map((data, i) => (
-            <Card key={data.title + "card"} {...data} />
-          ))) || <ReactLoading color="black" type="cylon" />}
-        {/* <div aria-label="Scroll right">
+
+            {(cardData.length &&
+              cardData.map((data, i) => (
+                <Card key={data.title + "card"} {...data} />
+              ))) || <ReactLoading color="black" type="cylon" />}
+            {/* <div aria-label="Scroll right">
           <FiChevronRight />
         </div> */}
-      </RowWrapper>
+          </RowWrapper>
+        </ColWrapper>
+      </ColWrapper>
     </CarouselWrapper>
   );
 };
@@ -33,14 +48,20 @@ const LeftArrow = styled.div`
 `;
 
 const CarouselWrapper = styled.div`
+  font-family: "Sofia Pro", "Helvetica Neue", Helvetica, Arial, sans-serif;
   background-color: #282c34;
-  padding: 35px 0;
+  width: 100%;
 `;
 
 const SectionHead = styled.h2`
   color: #a1a7b2;
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   font-weight: bold;
   text-transform: uppercase;
-  margin: 0 0 15px 70px;
+  margin-bottom: 15px;
+  align-self: flex-start;
+`;
+
+const ContentWrapper = styled.div`
+  margin: 0 auto;
 `;
